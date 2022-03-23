24c5b38273a46a24f4860681a55adf57031e9aa4
sujan2288 <64021023+sujan2288@users.noreply.github.com>
2022-03-24 10:53:27 +1100

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,9 @@
 
+const urlMetadata = require('url-metadata')
+urlMetadata('https://www.tiktok.com/@linhdan_9669/video/7077605707788356890?is_from_webapp=1&sender_device=pc&web_id=7075835694674052610').then(
+  function (metadata){
+     console.log(metadata)
+     },
+     function (error) {
+       console.log(error)
+     })
